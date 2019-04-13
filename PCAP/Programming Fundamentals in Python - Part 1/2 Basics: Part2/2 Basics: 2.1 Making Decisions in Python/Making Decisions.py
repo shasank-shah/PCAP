3b9bf2514817ed98774e8fb85f2ab7d33760c4f6
Shasank <shasank.shah@gmail.com>
2019-04-13 11:21:31 +0530

    Basics Part2

---
+++ PCAP/Programming Fundamentals in Python - Part 1/2 Basics: Part2/2 Basics: 2.1 Making Decisions in Python/Making Decisions.py
@@ -0,0 +1,13 @@
+a = 2
+b = 2.1
+
+if(a == b):
+    print("Values are equal")
+else:
+    print("Values are not equal")
+
+n = int(input("Enter value for n:"))
+if(n < 100):
+    print(False)
+else:
+    print(True)
