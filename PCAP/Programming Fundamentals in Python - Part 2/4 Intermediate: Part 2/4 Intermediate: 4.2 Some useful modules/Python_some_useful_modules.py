84c1316d8326d8681ff42f805bbefabe489cc9fe
Shasank <shasank.shah@gmail.com>
2019-05-20 12:37:45 +0530

    Create Python_some_useful_modules.py

---
+++ PCAP/Programming Fundamentals in Python - Part 2/4 Intermediate: Part 2/4 Intermediate: 4.2 Some useful modules/Python_some_useful_modules.py
@@ -0,0 +1,8 @@
+from math import pi, radians, degrees, sin, cos, tan
+ad = 90
+ar = radians(ad)
+ad = degrees(ar)
+print(ad == 90.)
+print(ar == pi/2.)
+print(sin(ar) / cos(ar) == tan(ar))
+print(asin(sin(ar)) == ar)
