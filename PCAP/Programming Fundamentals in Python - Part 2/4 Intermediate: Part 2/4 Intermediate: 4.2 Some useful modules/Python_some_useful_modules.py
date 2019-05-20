06e873dbfed04a5bbc507c9fbab7cb51ac8cfffb
Shasank <shasank.shah@gmail.com>
2019-05-20 12:43:31 +0530

    Update Python_some_useful_modules.py

--- PCAP/Programming Fundamentals in Python - Part 2/4 Intermediate: Part 2/4 Intermediate: 4.2 Some useful modules/Python_some_useful_modules.py
+++ PCAP/Programming Fundamentals in Python - Part 2/4 Intermediate: Part 2/4 Intermediate: 4.2 Some useful modules/Python_some_useful_modules.py
@@ -1,8 +1,15 @@
 from math import pi, radians, degrees, sin, cos, tan
+
 ad = 90
 ar = radians(ad)
 ad = degrees(ar)
 print(ad == 90.)
 print(ar == pi/2.)
 print(sin(ar) / cos(ar) == tan(ar))
-print(asin(sin(ar)) == ar)
+#print(asin(sin(ar)) == ar)
+
+from math import e, exp, log
+
+print(pow(e, 1) == exp(log(e)))
+print(pow(2, 2) == exp(2 * log(2)))
+print(log(e, e) == exp(0))
