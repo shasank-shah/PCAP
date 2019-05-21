432accf90ffba82a88cfd8f1b34260121484720f
Shasank <shasank.shah@gmail.com>
2019-05-21 22:39:36 +0530

    chapter 4.1 Python Modules  - 21/May/2019

---
+++ PCAP/Programming Fundamentals in Python - Part 2/4 Intermediate: Part 2/4 Intermediate: 4.8 The nature of strings in Python/Python_nature_of_strings.py
@@ -0,0 +1,31 @@
+word = 'by'
+print(len(word))
+
+empty = ''
+print(len(empty))
+
+I_am = 'I\'m'
+print(len(I_am))
+
+str1 = 'a'
+str2 = 'b'
+print(str1 + str2)
+print(str2 + str1)
+print(5 * 'a')
+print('b' * 4)
+
+c1 = 'a'
+print(ord(c1))
+
+print(chr(99))
+
+string = 'silly walks'
+for ix in range(len(string)):
+    print(string[ix], end=' ')
+print()
+
+lst = []
+for indx in range(len(string)):
+    lst.insert(0, string[indx])
+
+print(lst)
