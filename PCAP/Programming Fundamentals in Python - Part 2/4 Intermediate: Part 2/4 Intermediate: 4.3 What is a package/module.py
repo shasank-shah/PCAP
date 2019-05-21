d6c4c67ccc5546dabf3da6f0ea53215297cf88f3
Shasank <shasank.shah@gmail.com>
2019-05-21 11:30:11 +0530

    Update module.py

--- PCAP/Programming Fundamentals in Python - Part 2/4 Intermediate: Part 2/4 Intermediate: 4.3 What is a package/module.py
+++ PCAP/Programming Fundamentals in Python - Part 2/4 Intermediate: Part 2/4 Intermediate: 4.3 What is a package/module.py
@@ -1,2 +1,8 @@
 print("I like to be a module")
 print(_name_)
+
+counter = 0 # to count on times the module is being invoked
+if _name_ == "_main_":
+  print("I prefer to be a module")
+else:
+  print("I like to be a module")
