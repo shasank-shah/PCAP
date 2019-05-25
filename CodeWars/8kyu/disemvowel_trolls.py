705ca0db9637247fa7591aa4d238bec9aabb1811
Shasank <shasank.shah@gmail.com>
2019-05-25 10:24:13 +0530

    Delete disemvowel_trolls.py

--- CodeWars/8kyu/disemvowel_trolls.py
+++
@@ -1,9 +0,0 @@
-def shortcut( s ):
-    result_string = ''
-    for ch in s:
-        if ch in 'aeiouAEIOU':
-            ch = ''
-        result_string += ch
-    return result_string
-
-print(shortcut("Shasank SHAH"))
